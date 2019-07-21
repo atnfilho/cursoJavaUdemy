07aceeacd9cc8f4238a59b13d61f0d8c95a6785e
atnfilho <atnfilho@yahoo.com.br>
2019-07-21 09:28:15 -0300

    Aula 47 - Classe Product (Segundo problema em orientação a objetos)

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+       
+        Product product = new Product();
+        
+        System.out.println("Enter product data: ");
+        
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+        
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+                
+       
+    }
+
+}
