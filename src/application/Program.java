2c0e7194a478f4989e9bf48ca0106fc6345a19fa
atnfilho <atnfilho@yahoo.com.br>
2019-08-03 14:50:20 -0300

    Exercício da aula 62 - Seção 4 - Construtores, palavra this, sobrecarga, encapsulamento.

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,9 @@
  */
 package application;
 
+import entities.Conta;
 import java.util.Locale;
 import java.util.Scanner;
-import util.CurrencyConverter;
 
 /**
  *
@@ -20,18 +20,48 @@ public class Program {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Conta conta;
         
-        System.out.print("What is the dollar price? ");
-        double cotacao = sc.nextDouble();
-        System.out.print("How many dollar will be bought? ");
-        double qtde = sc.nextDouble();
+        System.out.print("Enter account number: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter account holder: ");
+        String holder = sc.nextLine();
         
-        double amount = CurrencyConverter.dollarToReal(cotacao, qtde);
+        System.out.println("Is there an initial deposit? y/n");
+        char resposta = sc.next().charAt(0);
         
-        System.out.printf("Amount to be paid in reais: %.2f%n", amount);
+        if(resposta == 'y' || resposta == 'Y'){
+            System.out.print("Enter initial deposit value: ");
+            double deposit = sc.nextDouble();
+            conta = new Conta(numero, holder, deposit);
+        } else {
+            conta = new Conta(numero, holder);
+        }
+        
+        System.out.println("=============================================");
+        System.out.println("Dados da Conta:");
+        System.out.println(conta);
+        System.out.println();
+        System.out.println("Enter a deposit value: ");
+        double deposit = sc.nextDouble();
+        conta.deposita(deposit);
+        
+        System.out.println("=============================================");
+        System.out.println("Conta Atualizada:");
+        System.out.println(conta);
+        
+        System.out.println();
+        System.out.println("Enter a withdraw value:");
+        double saque = sc.nextDouble();
+        conta.saca(saque);
+        
+        System.out.println("=============================================");
+        System.out.println("Conta Atualizada:");
+        System.out.println(conta);
     }
 
 }
