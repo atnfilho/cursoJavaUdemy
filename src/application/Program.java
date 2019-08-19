cee8462a0e191472539f614fb0613450c89d51ce
atnfilho <atnfilho@yahoo.com.br>
2019-08-19 19:20:36 -0300

    Aula 70 - Vetores 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@
  */
 package application;
 
-import entities.Conta;
+import entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -21,47 +21,30 @@ public class Program {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        Conta conta;
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+       
+       int n = sc.nextInt();
+       Product[] vect = new Product[n];
+       
+       for(int i=0; i<vect.length; i++)
+       {
+           sc.nextLine();
+           String name = sc.nextLine();
+           double price = sc.nextDouble();
+           vect[i] = new Product(name, price);
+       }
+       
+       double sum = 0.0;
+       for(int i=0; i<vect.length; i++)
+       {
+           sum += vect[i].getPrice();
+       }
+       
+       double avg = sum / vect.length;
+       
+        System.out.printf("AVERAGE PRICE: %.2f%n", avg);
         
-        System.out.print("Enter account number: ");
-        int numero = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter account holder: ");
-        String holder = sc.nextLine();
-        
-        System.out.println("Is there an initial deposit? y/n");
-        char resposta = sc.next().charAt(0);
-        
-        if(resposta == 'y' || resposta == 'Y'){
-            System.out.print("Enter initial deposit value: ");
-            double deposit = sc.nextDouble();
-            conta = new Conta(numero, holder, deposit);
-        } else {
-            conta = new Conta(numero, holder);
-        }
-        
-        System.out.println("=============================================");
-        System.out.println("Dados da Conta:");
-        System.out.println(conta);
-        System.out.println();
-        System.out.println("Enter a deposit value: ");
-        double deposit = sc.nextDouble();
-        conta.deposita(deposit);
-        
-        System.out.println("=============================================");
-        System.out.println("Conta Atualizada:");
-        System.out.println(conta);
-        
-        System.out.println();
-        System.out.println("Enter a withdraw value:");
-        double saque = sc.nextDouble();
-        conta.saca(saque);
-        
-        System.out.println("=============================================");
-        System.out.println("Conta Atualizada:");
-        System.out.println(conta);
     }
 
 }
