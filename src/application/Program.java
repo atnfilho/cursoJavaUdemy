aff13a63578c7f65efcfdf62c9fab87e1db6626d
atnfilho <atnfilho@yahoo.com.br>
2019-08-31 23:27:15 -0300

    Aula 76 - exercício sobre listas

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@
  */
 package application;
 
-import entities.Product;
+import entities.Employee;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -20,31 +22,52 @@ public class Program {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-       Locale.setDefault(Locale.US);
-       Scanner sc = new Scanner(System.in);
-       
-       int n = sc.nextInt();
-       Product[] vect = new Product[n];
-       
-       for(int i=0; i<vect.length; i++)
-       {
-           sc.nextLine();
-           String name = sc.nextLine();
-           double price = sc.nextDouble();
-           vect[i] = new Product(name, price);
-       }
-       
-       double sum = 0.0;
-       for(int i=0; i<vect.length; i++)
-       {
-           sum += vect[i].getPrice();
-       }
-       
-       double avg = sum / vect.length;
-       
-        System.out.printf("AVERAGE PRICE: %.2f%n", avg);
-        
-    }
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many employes will be registered?");
+        int n = sc.nextInt();
+
+        List<Employee> list = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+
+            Employee employee = new Employee();
+
+            System.out.println("Employee #" + (i + 1));
+            System.out.print("ID: ");
+            employee.setId(sc.nextInt());
+            sc.nextLine();
+            System.out.println("Name: ");
+            employee.setName(sc.nextLine());
+            System.out.println("Salary: ");
+            employee.setSalary(sc.nextDouble());
+            sc.nextLine();
+            
+            list.add(employee);
+        }
+
+        System.out.println("Enter the employee id that will have salary increase: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        
+        if(emp == null) {
+            System.out.println("This id does not exist!");
+        } else {
+            System.out.println("Enter the percentage: ");
+            double percentage = sc.nextDouble();
+            emp.increaseSalary(percentage);
+        }
+        
+        
+        
+        System.out.println("List of employees: ");
+        for(Employee employee : list) {            
+            System.out.println(employee);
+        }
+
+    }
 }
