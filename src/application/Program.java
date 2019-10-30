63cce7007ff652f836540962ddfba6c9e517cf0e
atnfilho <atnfilho@yahoo.com.br>
2019-10-29 23:10:38 -0300

    Aula 123 - Sobreposição, palavra SUPER, Override

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,33 +20,16 @@ public class Program {
      */
     public static void main(String[] args) {
      
-        Account acc = new Account(1001, "Alex", 0.0);
-        BusinessAccount bacc = new BusinessAccount(500.0, 1002, "Maria", 0.0);
-        SavingsAccount sacc = new SavingsAccount(0.01, 1003, "Renata", 0.0);
-                
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
         
-        // UPCASTING
-
-        Account acc1 = bacc; // business account é uma conta
-        Account acc2 = sacc; // savings account é uma conta
+        Account acc2 = new BusinessAccount(500.0, 1002, "Maria", 1000.0);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
         
-        
-        // DOWNCASTING
-        
-        BusinessAccount acc3 = (BusinessAccount)acc1;
-        acc3.loan(200.0);
-        
-        if(acc2 instanceof BusinessAccount)
-        {
-            System.out.println("yes");
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-        }
-        else
-        {
-            System.out.println("no");
-            SavingsAccount acc4 = (SavingsAccount)acc2;
-        }
-        
-        
-     }
+        Account acc3 = new SavingsAccount(0.01, 1003, "Bob", 1000.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+    }
 }
--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -33,4 +33,9 @@ public class SavingsAccount extends Account {
     public void updateBalance() {
         balance += balance * interestRate;
     }
+    
+    @Override
+    public void withdraw(Double amount) {
+        balance -= amount;
+    }
 }
