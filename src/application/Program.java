a6c27e7e554e972044973b137a096776834cfba7
atnfilho <atnfilho@yahoo.com.br>
2019-11-14 20:25:38 -0300

    Aula 177 - FileReader e BufferedReader, bloco try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,10 @@
  */
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -23,20 +25,17 @@ public class Program {
      */
     public static void main(String[] args) {
         
-        File file = new File("C:\\Temp\\in.txt");
-        Scanner sc = null;
-        
-        try {
-            sc = new Scanner(file);
-            while(sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+        String path = "c:\\temp\\in.txt";
+        // try-with-resources
+        // fecha os objetos ao final da rotina
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while(line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error");
-        } finally {
-            if(sc != null) {
-                sc.close();
-            }
-        }
+        } 
     }
 }
